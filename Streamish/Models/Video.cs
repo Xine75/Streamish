eb77a6e1e8f3ef02a7d83ed4ae4f967d779ae435
Xine <cjdoza@gmail.com>
2021-07-06 13:56:41 -0500

    initial

---
+++ Streamish/Models/Video.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+
+namespace Streamish.Models
+{
+    public class Video
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string ImageUrl { get; set; }
+
+        public string Caption { get; set; }
+
+        public DateTime DateCreated { get; set; }
+
+        public int UserProfileId { get; set; }
+
+        public UserProfile UserProfile { get; set; }
+
+        public List<Comment> Comments { get; set; }
+
+    }
+}
