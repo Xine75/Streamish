953d33bb688058b280c4c0a2c05ecc38bd6b4d22
Xine <cjdoza@gmail.com>
2021-07-08 11:49:28 -0500

    search hottest

--- Streamish/Repositories/VideoRepository.cs
+++ Streamish/Repositories/VideoRepository.cs
@@ -372,6 +372,53 @@ namespace Streamish.Repositories
                 }
             }
         }
+        public List<Video> SearchForHottest (DateTime datetime)
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    var sql = @"
+                    SELECT v.Id, v.Title, v.Description, v.Url, v.DateCreated AS VideoDateCreated, v.UserProfileId,
 
+                     up.Name, up.Email, up.DateCreated AS UserProfileDateCreated,
+                     up.ImageUrl AS UserProfileImageUrl
+                        
+                FROM Video v 
+                     JOIN UserProfile up ON v.UserProfileId = up.Id
+               WHERE v.DateCreated >= @DateTime";
+
+                    cmd.CommandText = sql;
+                    DbUtils.AddParameter(cmd, "@DateTime", datetime);
+                    var reader = cmd.ExecuteReader();
+
+                    var videos = new List<Video>();
+                    while (reader.Read())
+                    {
+                        videos.Add(new Video()
+                        {
+                            Id = DbUtils.GetInt(reader, "Id"),
+                            Title = DbUtils.GetString(reader, "Title"),
+                            Description = DbUtils.GetString(reader, "Description"),
+                            DateCreated = DbUtils.GetDateTime(reader, "VideoDateCreated"),
+                            Url = DbUtils.GetString(reader, "Url"),
+                            UserProfileId = DbUtils.GetInt(reader, "UserProfileId"),
+                            UserProfile = new UserProfile()
+                            {
+                                Id = DbUtils.GetInt(reader, "UserProfileId"),
+                                Name = DbUtils.GetString(reader, "Name"),
+                                Email = DbUtils.GetString(reader, "Email"),
+                                DateCreated = DbUtils.GetDateTime(reader, "UserProfileDateCreated"),
+                                ImageUrl = DbUtils.GetString(reader, "UserProfileImageUrl"),
+                            },
+
+                        });
+                    }
+                    reader.Close();
+                    return videos;
+                }
+            }
+        }
     }
 }
