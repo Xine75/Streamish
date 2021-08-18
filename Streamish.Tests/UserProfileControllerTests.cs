744330c72d5c9c7bb3caf534b7cfe1150da96797
Xine <cjdoza@gmail.com>
2021-08-18 17:41:48 -0500

    tests written

--- Streamish.Tests/UserProfileControllerTests.cs
+++ Streamish.Tests/UserProfileControllerTests.cs
@@ -71,6 +71,110 @@ namespace Streamish.Tests
             Assert.Equal(testUserId, actualUser.Id);
         }
 
+        [Fact]
+        public void Post_Method_Adds_A_New_User()
+        {
+            //Arrange
+            var userCount = 20;
+            var users = CreateTestUsers(userCount);
+
+            var repo = new InMemoryUserRepository(users);
+            var controller = new UserProfileController(repo);
+
+            //Act
+            var newUser = new UserProfile()
+            {
+                Name = "Name",
+                Email = "Email",
+                ImageUrl = "http://userProfile.url/",
+                DateCreated = DateTime.Today,
+            };
+
+            controller.Post(newUser);
+
+            //Assert
+            Assert.Equal(userCount + 1, repo.InternalData.Count);
+        }
+        [Fact]
+        public void Put_Method_Returns_BadRequest_When_Ids_Do_Not_Match()
+        {
+            //Arrange
+            var testUserId = 99;
+            var users = CreateTestUsers(5);
+            users[0].Id = testUserId; //Make sure we know the Id of one of the users
+
+            var repo = new InMemoryUserRepository(users);
+            var controller = new UserProfileController(repo);
+
+            var userToUpdate = new UserProfile()
+            {
+                Id = testUserId,
+                Name = "Updated!",
+                Email = "Updated!",
+                ImageUrl = $"http://some.url/",
+                DateCreated = DateTime.Today,
+            };
+            var someOtherUserId = testUserId + 1; //make sure they aren't the same
+
+            //Act
+            var result = controller.Put(someOtherUserId, userToUpdate);
+
+            //Assert
+            Assert.IsType<BadRequestResult>(result);
+        }
+
+        [Fact]
+        public void Put_Method_Updates_A_User()
+        {
+            //Arrange
+            var testUserId = 99;
+            var users = CreateTestUsers(5);
+            users[0].Id = testUserId;
+
+            var repo = new InMemoryUserRepository(users);
+            var controller = new UserProfileController(repo);
+
+            var userToUpdate = new UserProfile()
+            {
+                Id = testUserId,
+                Name = "Updated!",
+                Email = "Updated!",
+                ImageUrl = $"http://some.url/",
+                DateCreated = DateTime.Today,
+            };
+
+            //Act
+            controller.Put(testUserId, userToUpdate);
+
+            //Assert
+            var userFromDb = repo.InternalData.FirstOrDefault(u => u.Id == testUserId);
+            Assert.NotNull(userFromDb);
+
+            Assert.Equal(userToUpdate.Name, userFromDb.Name);
+            Assert.Equal(userToUpdate.Email, userFromDb.Email);
+            Assert.Equal(userToUpdate.ImageUrl, userFromDb.ImageUrl);
+            Assert.Equal(userToUpdate.DateCreated, userFromDb.DateCreated);
+        }
+
+        [Fact]
+        public void Delete_Method_Removes_A_User()
+        {
+            //Arrange
+            var testUserId = 99;
+            var users = CreateTestUsers(5);
+            users[0].Id = testUserId;
+
+            var repo = new InMemoryUserRepository(users);
+            var controller = new UserProfileController(repo);
+
+            //Act
+            controller.Delete(testUserId);
+
+            //Assert
+            var userFromDb = repo.InternalData.FirstOrDefault(u => u.Id == testUserId);
+            Assert.Null(userFromDb);
+        }
+
         private List<UserProfile>CreateTestUsers(int count)
         {
             var users = new List<UserProfile>();
